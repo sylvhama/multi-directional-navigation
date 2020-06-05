aca9b65c0fd3c1aa82385c8e4ba0ea16e66ef7d4
Sylvain Hamann <sylvain.hamann@shopify.com>
2020-06-04 20:49:05 -0400

    fix style

--- src/components/App/components/Nav/components/List/List.tsx
+++ src/components/App/components/Nav/components/List/List.tsx
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export const List = styled.ul`
   display: grid;
   grid-gap: 1rem;
-  grid-template-columns: repeat(3, auto);
+  grid-template-columns: repeat(3, 1fr);
   text-align: center;
 `;
