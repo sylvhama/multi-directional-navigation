5ea58bf3017459fc93f285e90e8017da43eb8b4c
Sylvain Hamann <sylvain.hamann@shopify.com>
2020-06-10 13:30:31 -0400

    still flaky

--- cypress/integration/routing.js
+++ cypress/integration/routing.js
@@ -4,16 +4,16 @@ describe("Routing", () => {
     cy.pressM();
   });
 
+  // We press up twice to make the test not flaky until I find better solution :/
   it("visits each page", () => {
-    cy.pressUp().pressRight();
+    cy.pressUp().pressUp().pressRight();
     cy.focused().click();
     cy.url().should("include", "/horizontal-list");
 
-    cy.pressUp().pressRight().pressRight();
+    cy.pressUp().pressUp().pressRight().pressRight();
     cy.focused().click();
     cy.url().should("include", "/modal");
 
-    // We press up twice to make the test not flaky until I find better solution :/
     cy.pressUp().pressUp();
     cy.focused().click();
     cy.url().should("eq", Cypress.config().baseUrl);
