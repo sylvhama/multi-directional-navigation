b93b3659891096cc913bdc0bd87a205397022e95
Sylvain Hamann <sylvain.hamann@shopify.com>
2020-06-10 11:46:54 -0400

    typo

--- cypress/integration/routing.js
+++ cypress/integration/routing.js
@@ -13,7 +13,7 @@ describe("Routing", () => {
     cy.focused().click();
     cy.url().should("include", "/modal");
 
-    // We press up twice to make the test not flaky until I find better solutiom :/
+    // We press up twice to make the test not flaky until I find better solution :/
     cy.pressUp().pressUp();
     cy.focused().click();
     cy.url().should("eq", Cypress.config().baseUrl);
